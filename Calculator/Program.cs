5458ac97978f8f3915e2051b405310d6b25b0c1f
TLuht <thomas.luht@tktk.ee>
2023-12-19 23:37:40 +0200

    arguments and parameters

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter the operation (+/-)");
+char userOperator = char.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number A");
+int firstNum = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Enter number B");
+int secondNum = Int32.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+    case '+':
+        Addition(firstNum, secondNum); 
+        break;
+    case '-':
+        Subtraction(firstNum, secondNum);
+        break;
+    default:
+        Console.WriteLine("Error with input");
+        break;
+}
+
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+
+static void Subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
---
+++ HindelineTest7/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Convert temperature to Celsius or Fahrenheit? (C/F)");
+char userChoice = char.ToUpper(char.Parse(Console.ReadLine()));
+
+Console.WriteLine("Insert temperature");
+int userInput = Int32.Parse(Console.ReadLine());
+
+switch(userChoice)
+{
+    case 'C':
+        ConvertToCelsius(userInput);
+        break;
+    case 'F':
+        ConvertToFahrenheit(userInput);
+        break;
+    default:
+        Console.WriteLine("Error, wrong conversion unit selected");
+        break;
+}
+
+static void ConvertToCelsius(int userInput)
+{
+    Console.WriteLine($"{(userInput - 32) * 5 / 9} kraadi ");
+}
+
+static void ConvertToFahrenheit(int userInput)
+{
+    Console.WriteLine($"{(userInput * 9) / 5 + 32} kraadi");
+}
